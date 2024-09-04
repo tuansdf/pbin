6eb2e8afe1ba526cf29c152e2738afa23fd14f66
tuansdf <103062682+tuansdf@users.noreply.github.com>
2024-09-05 00:33:47 +0700

    update

--- src/shared/schemas/common.schema.ts
+++ src/shared/schemas/common.schema.ts
@@ -5,6 +5,7 @@ export const passwordSchema = z
     required_error: "Password is required",
     invalid_type_error: "Invalid password",
   })
-  .min(12, "Must have at least 12 characters");
+  .min(12, "Must have at least 12 characters")
+  .max(128, "Must have at most 128 characters");
 
 export const stringOrUndefined = z.string().transform((v) => v || undefined);
