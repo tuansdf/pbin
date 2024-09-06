e17450c0c151004721e2769fdb88fd185a62b69a
tuansdf <103062682+tuansdf@users.noreply.github.com>
2024-09-07 01:38:17 +0700

    update

--- src/shared/utils/common.util.ts
+++ src/shared/utils/common.util.ts
@@ -7,7 +7,7 @@ export const generateFakeContent = (base: string): string => {
   // plus 2 in case not found, divide by 6 to reduce the number of rounds
   const rounds = Math.ceil((base64urlAlphabet.indexOf(result[0]) + 2) / 6);
   for (let i = 0; i < rounds; i++) {
-    result += createHash(result.substring(0, 10)); // shorten the hash input
+    result += createHash(result[i]); // shorten the hash input
   }
   return result;
 };
