11d845a977f8016f6313016d8f1aa893bb72cc90
tuansdf <103062682+tuansdf@users.noreply.github.com>
2024-09-07 12:24:42 +0700

    update

--- src/server/entities/vault.entity.ts
+++ src/server/entities/vault.entity.ts
@@ -1,13 +1,19 @@
 import { generateId } from "@/shared/utils/crypto";
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { index, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const VaultTable = sqliteTable("vault", {
-  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-  publicId: text("public_id", { mode: "text", length: 128 })
-    .unique()
-    .$defaultFn(() => generateId()),
-  content: text("content", { mode: "text", length: 20000 }),
-  masterPassword: text("master_password", { mode: "text", length: 128 }),
-  configs: text("configs", { mode: "text", length: 512 }),
-  expiresAt: integer("expires_at", { mode: "number" }),
-});
+export const VaultTable = sqliteTable(
+  "vault",
+  {
+    id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    publicId: text("public_id", { mode: "text", length: 128 })
+      .unique()
+      .$defaultFn(() => generateId()),
+    content: text("content", { mode: "text", length: 20000 }),
+    masterPassword: text("master_password", { mode: "text", length: 128 }),
+    configs: text("configs", { mode: "text", length: 512 }),
+    expiresAt: integer("expires_at", { mode: "number" }),
+  },
+  (table) => ({
+    expiresAtIdx: index("vault_expires_at_idx").on(table.expiresAt),
+  }),
+);
